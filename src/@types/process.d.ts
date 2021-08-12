04add57fc253b8a80bca893a7419b2b46e4bb82c
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-08-11 21:08:43 -0300

    🏷️ declare environment variables types

---
+++ src/@types/process.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_PIPEFY_API_URL?: string;
+    REACT_APP_PIPEFY_API_TOKEN?: string;
+    REACT_APP_PIPEFY_ORGANIZATION_ID?: string;
+  }
+}
