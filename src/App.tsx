3509be5795fe5f2389671d3208fac9be96bfe574
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-08-11 21:13:31 -0300

    🎨 define functions returns types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import type { ReactElement } from 'react';
 import './App.css';
 
-function App() {
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
